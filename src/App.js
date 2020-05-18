44f2339566f58bb938dfd069ee69afc58f7e0234
Sandeep Devalla <sandeepdevalla@Sandeeps-MacBook-Pro.local>
2020-05-18 10:50:51 +0530

    added marquee tag for head scrolling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Welcome to Air India Flight Booking Services
+         Welcome to Air India Flight Booking Services
+        <div>
+        <marquee>For Emergency You Can Call to Toll Free No 1800-425-428 </marquee>
+        </div>
+
       </header>
       <Dashboard />
       <footer className="App-footer">
